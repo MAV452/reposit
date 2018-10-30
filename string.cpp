28a17232187febb0a05d35bc5d020499842f1548
TNAN <pilot-452@mail.ru>
2018-10-30 13:14:56 +0700

    back where we were

--- string.cpp
+++ string.cpp
@@ -10,7 +10,7 @@
 int _tmain(int argc, _TCHAR* argv[])
 {
 setlocale(0,"");
-char str[]="dog milk city street train  swan bird strangest danger";
+char str[]="dog milk city street train  swan bird danger";
 char ch;
 printf("Исходная строка:\n%s \nВведите символ для поиска.\n",str);
 scanf_s("%c",&ch);
