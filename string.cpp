b43a4274c8374a82bec3c77339ef1a3cd0438f53
TNAN <pilot-452@mail.ru>
2018-10-30 13:04:20 +0700

    comment on commit

---
+++ string.cpp
@@ -0,0 +1,44 @@
+// string.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+setlocale(0,"");
+char str[]="dog milk city street train  swan bird danger ";
+char ch;
+printf("Исходная строка:\n%s \nВведите символ для поиска.\n",str);
+scanf_s("%c",&ch);
+printf_s("Вы ввели символ: %c\n",ch);
+int i=0;
+int leng=strlen(str);
+while (i<leng)
+{
+	if(ch==str[i]){
+		if (i!=0){
+		while(str[i]!=' '){
+			i--;
+	}
+		if(str[i]==' '){
+			i++;
+		}
+		}
+		while((str[i]!=' ')){
+			printf("%c",str[i]);
+			i++;
+		} 
+		printf(" ");
+		i++;
+	} else {
+		i++;
+	}
+	}
+
+	return 0;
+}
+
